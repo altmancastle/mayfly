e80cb304b112040393cf5a1d4f1146516fc7336b
hanbiao <hanbiao4079@qq.com>
2023-03-18 10:32:57 +0800

    init

--- packages/ui/src/button/button.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Button = () => {
-  return <div>测试button3333</div>;
-};
-
-export default Button;
--- packages/ui/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as MfButton } from "./button/index";
